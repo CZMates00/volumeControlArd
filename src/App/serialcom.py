eed67dbc442042544a22e9014a0b3b6815570b10
Matěj Sobotka <sobotma7@fit.cvut.cz>
2024-05-09 12:28:55 +0200

    dev-win: serialcom.py: added serialPortList() function for correct port selection

--- src/App/serialcom.py
+++ src/App/serialcom.py
@@ -1,4 +1,5 @@
 import serial
+import serial.tools.list_ports
 
 # serial communication class
 class CSerial:
@@ -20,3 +21,23 @@ class CSerial:
 
     def close(self):
         self.m_ser.close()
+
+# returns chosen serial port name
+def serialPortList():
+    ports = serial.tools.list_ports.comports()
+    portStr = []
+    while True:
+        print("\n-----\n")
+        num = 0
+        for port, desc, hwid in sorted(ports):
+            print(f"{num}: {port}: {desc}")
+            portStr.append(str(port))
+            num += 1
+        num -= 1
+        try:
+            choice = int(input(f"Select serial port by number 0 to {num}: "))
+        except ValueError: print("Invalid number!"); continue
+        if choice > num or choice < 0 or type(choice) is not int: print("Invalid number!"); continue
+        else: break 
+
+    return portStr[choice]
