1697324d90c3db3e42b58a9919297a2e2b02bd0a
Matěj Sobotka <sobotma7@fit.cvut.cz>
2024-05-09 12:33:09 +0200

    dev-win: winvol.py: added methods for interfacing the master audio output in native (%) format

--- src/App/winvol.py
+++ src/App/winvol.py
@@ -1,5 +1,6 @@
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume, IAudioEndpointVolume
+from math import log, pow
 
 # Windows volume control
 class CVolume():
@@ -44,3 +45,46 @@ class CVolume():
     # set master volume using native format as specified above
     def setMasterVolume(self, volume):
         self.m_master_volume.SetMasterVolumeLevel(volume, None)
+
+    # master volume getter in % format in range <0, 100>
+    # inverse to logarithmic function used for volume setting
+    def getMasterVolumeNative(self):
+        self.cur = self.getMasterVolume()
+        if self.cur == -65.25: return 0
+        elif self.cur >= -65 and self.cur < -55 : return round(pow(10, 2+(self.cur/29)))
+        elif self.cur >= -55 and self.cur < -46 : return round(pow(10, 2+(self.cur/31.2)))
+        elif self.cur >= -46 and self.cur < -41 : return round(pow(10, 2+(self.cur/32.5)))
+        elif self.cur >= -41 and self.cur < -34 : return round(pow(10, 2+(self.cur/33)))
+        elif self.cur >= -34 and self.cur < -28 : return round(pow(10, 2+(self.cur/33.5)))
+        elif self.cur >= -28 and self.cur < 0 : return round(pow(10, 2+(self.cur/34)))
+        elif self.cur == 0.0: return 100
+        else: raise Exception("Invalid volume level returned!")
+
+    # set master volume using % format as specified above
+    # approximation of a logarithmic function for platforms with native format range <-65.25, 0>
+    def setMasterVolumeNative(self, volume):   
+        self.min_db = -65.25
+        self.max_db = 0
+        if (type(volume) is not int): raise TypeError("Volume must be int type!")
+        if volume <= 0: new_volume_to_set = self.min_db
+        elif volume < 2 and volume > 0: new_volume_to_set = 29*log(volume/100, 10)
+        elif volume < 4 and volume >= 2: new_volume_to_set = 31.2*log(volume/100, 10)
+        elif volume < 6 and volume >= 4: new_volume_to_set = 32.5*log(volume/100, 10)
+        elif volume < 10 and volume >= 6: new_volume_to_set = 33*log(volume/100, 10)
+        elif volume < 15 and volume >= 10: new_volume_to_set = 33.5*log(volume/100, 10)
+        elif volume < 100 and volume >= 15: new_volume_to_set = 34*log(volume/100, 10)
+        elif volume >= 100: new_volume_to_set = self.max_db
+        else: raise Exception("Invalid volume value!"); return
+
+        self.setMasterVolume(new_volume_to_set)
+        return new_volume_to_set
+
+    # increment master volume by number in native format
+    def setMasterVolumeIncNative(self, volInc):
+        cur = self.getMasterVolumeNative()
+        self.setMasterVolumeNative(cur+volInc)
+
+    # decrement master volume by number in native format
+    def setMasterVolumeDecNative(self, volDec):
+        cur = self.getMasterVolumeNative()
+        self.setMasterVolumeNative(cur-volDec)
